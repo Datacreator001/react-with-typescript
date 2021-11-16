31c1d116d19c160a8aaabeec7196d8ab7ecd8ebf
datacreator001 <amirrorphone@gmail.com>
2021-11-16 05:49:26 -0500

    Set up Interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List'
+
+interface IState {
+	people: {
+		name: string;
+		age: number;
+		url: string;
+		note?: string;
+	}[];
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [people, setPeople] = useState<IState["people"]>([]);
+
+  
+	return (
+		<div className='App'>
+			<h1>People invited to my Party</h1>
+      <List people={people}/>
+		</div>
+	);
 }
 
 export default App;
