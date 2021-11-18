0b62ec7a2c6aaafcd9f74fddd56724ee420a2ee3
datacreator001 <amirrorphone@gmail.com>
2021-11-18 13:59:27 -0500

    Added event handlers using typescript syntax

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import List from './components/List'
+import AddToList from './components/AddToList';
 
 interface IState {
 	people: {
@@ -16,7 +17,7 @@ function App() {
 	const [people, setPeople] = useState<IState['people']>([
 		{
 			name: 'Lebron James',
-			url: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fhoopshype.com%2Fwp-content%2Fuploads%2Fsites%2F92%2F2021%2F10%2Fi_0d_d7_16_lebron-james.png&imgrefurl=https%3A%2F%2Fhoopshype.com%2F2021%2F11%2F15%2Flebron-james-progressing-great-from-injury%2F&tbnid=5bRW_KEW1w3EUM&vet=12ahUKEwivs9v_25z0AhW2wJcIHXapAIQQMygAegUIARDKAQ..i&docid=LT5DOtZLIFKZHM&w=1280&h=900&itg=1&q=lebron%20james&ved=2ahUKEwivs9v_25z0AhW2wJcIHXapAIQQMygAegUIARDKAQ',
+			url: 'https://sportshub.cbsistatic.com/i/r/2021/10/15/4744c60f-62e7-4c18-968e-0d21934f0681/thumbnail/1200x675/21ce949aa64f20666919da58bb15986e/lebron-james.jpg',
 			age: 36,
 			note: 'Allergic to staying on the same team',
 		},
@@ -27,6 +28,7 @@ function App() {
 		<div className='App'>
 			<h1>People invited to my Party</h1>
       <List people={people}/>
+	  <AddToList />
 		</div>
 	);
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -10,13 +10,24 @@ interface IProps {
 }
 
 const List: React.FC<IProps> = ({people}) => {
+    
+    const renderList =()=>{
+        return people.map((person)=>{
+            return(
+           <li className="List">
+               <div className="List-header">
+                   <img src={person.url} alt="No Image" className="List-img" />
+                   <h2>{person.name}</h2>
+               </div>
+               <p>{person.age} years old</p>
+               <p className="List-note">{person.note}</p>
+           </li>
+        )})
+    }
+
     return (
     <ul>
-       {people.map(person=>{
-           return (
-               <div>{person.name}</div>
-           )
-       })} 
+       {renderList()}
     </ul>
     )
 }
